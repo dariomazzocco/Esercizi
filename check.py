4e6c36c2ecc9f6237ca1127aa90cf6b0db0e7736
dariomazzocco <56355108+dariomazzocco@users.noreply.github.com>
2020-02-20 15:15:28 +0100

    Modifica di check.py

---
+++ check.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 20 14:31:42 2020
+
+@author: macbookdario
+"""
+
+import tensorflow as tf
+import keras as k
+import pandas as pd
+import numpy as np #importa i vettori
+import sklearn as sc
+import matplotlib as mb
+import pdb
+
+print("ciao")
